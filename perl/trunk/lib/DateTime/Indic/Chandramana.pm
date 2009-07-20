f2d3808536fa03a136238c2fd7e4e513f16783a5
jaldhar.vyas <jaldhar.vyas@372c777b-1b26-0410-a920-ef5be020fda2>
2009-07-20 03:07:49 +0000

    Added many more methods to Chandramana.pm.  Documented all methods. Rearranged some of the locale related stuff.  perltidy'ed up.

--- perl/trunk/lib/DateTime/Indic/Chandramana.pm
+++ perl/trunk/lib/DateTime/Indic/Chandramana.pm
@@ -5,7 +5,7 @@ package DateTime::Indic::Chandramana;
 use warnings;
 use strict;
 use Carp qw/ carp croak /;
-use DateTime::Event::Lunar qw/ :phases /;
+use DateTime::Event::Lunar;
 use DateTime::Indic::Utils qw/ epoch sidereal_year sidereal_month calendar_year
   lunar_day lunar_on_or_before solar_longitude zodiac /;
 use DateTime::Event::Sunrise;
@@ -20,11 +20,11 @@ DateTime::Indic::Chandramana - Base class for Indian luni-solar calendars
 
 =head1 VERSION
 
-Version 0.01
+Version 0.2
 
 =cut
 
-our $VERSION = '0.01';
+our $VERSION = '0.2';
 
 =head1 SYNOPSIS
 
@@ -41,11 +41,30 @@ L<DateTime::Indic> collection.
 
 =cut
 
-my @masa_nama = qw{ chaitra vaishAka jyeShTa AShADha shrAvaNa bhAdrapada
-  ashvina kArtika mArgashIrasa pauSha mAgha phAlguna
+my @varsha_nama = qw{ prabhava vibhava shukla pramoda prajApati a~ngirA
+  shrImukha bhAva yuvA dhAtA ishvara bahudhAnya pramAthI vikrama vR^isha
+  chitrabhAnu subhAnu tAraNa pArthiva vyaya sarvajit sarvadhArI virodhI
+  vikrti khara nandana vijaya jaya manmatha durmukha hemalambi vilambi
+  vikArI shArvarI plava shubhakrta shobhana krodhI vishvavAsu parAbhava
+  plava~nga kIlaka saumya sAdharaNa virodhakrta paridhAvi pramAdi Ananda
+  rAkshasa anala pi~Ngala kAlayukta siddhArthi raudra durmati dundubhi
+  rudhirodgArI raktAkShI krodhana kshaya
 };
 
-my @masa_abbrev = qw{ chai vai jye AshA shrA bhA a kA  mAr pau mA phA };
+my @vara_nama = qw{
+  ravivAra somavAra ma~ngalavAra budhavAra guruvAra shukravAra shanivAra
+};
+
+my @vara_abbrev = qw { ra so ma bu gu shu sha };
+
+my @masa_nama = qw{
+  chaitra vaishAka jyeShTa AShADha shrAvaNa bhAdrapada ashvina kArtika
+  mArgashIrasa pauSha mAgha phAlguna
+};
+
+my @masa_abbrev = qw{
+  chai vai jye AshA shrA bhA a kA  mAr pau mA phA
+};
 
 my @paksha_nama = qw{shukla kR^iShNa};
 
@@ -53,11 +72,79 @@ my @paksha_abbrev = qw{shu kR^i};
 
 my $adhika_nama = 'adhika';
 
+my $adhika_abbrev = 'a';
+
+my @tithi_nama = qw{
+  pratipadA dvitIyA tR^itIya chaturthI paMchamI ShaShTI saptamI aShTamI
+  navamI dashamI ekAdashI dvAdashI trayodashI chaturdashI
+};
+
+my @tithi_abbrev = qw{
+  pra dvi tR^i cha paM Sha sa a na da e dvA tra chada
+};
+
+my $amavasya_nama = 'amAvAsyA';
+
+my $amavasya_abbrev = 'a';
+
+my $purnima_nama = 'pUrNimA';
+
+my $purnima_abbrev = 'pU';
+
 =head1 METHODS
 
-=head2 new (%args)
+=head2 DATETIME METHODS
 
-TODO
+These methods are either required by the L<DateTime> API or copied from it.
+
+=head3 new (%args)
+
+Constructs a new instance of this class.  The following arguments can be given:
+
+=over 4
+
+=item varsha
+
+The numeric year according to the calender's era.  Defaults to 0.
+
+=item masa
+
+The mAsa (lunar month) as a number from 1 to 12.  Defaults to 1.
+
+=item adhikamasa
+
+1 if this is an adhika (leap) mAsa, 0 otherwise.  Defaults to 0.
+
+=item paksha
+
+1 if this is the kR^iShNapakSha (waning half)  of a mAsa, 0 if it is the
+shuklapakSha (waxing half.)  Defaults to 0.
+
+=item tithi
+
+The numeric tithi (lunar day) expressed as a number from 1 to 14 or by 
+convention, 15 for the pUrNimA (full moon) and 30 for the amAvAsya (new moon.) 
+Defaults to 1.
+
+=item adhikatithi
+
+1 if this is an adhika (leap) tithi, 0 otherwise.  Defaults to 0.
+
+=item latitude
+
+The latitude of the point for which the panchanga is to be calculated 
+expressed as decimal degrees.  Negative values are used for latitudes south of 
+the equator so the allowable range for this argument is from -180 to 180.  
+Defaults to 23.15, the latitude of avantika.
+
+=item longitude
+
+The longitude of the point for which the panchanga is to be calculated 
+expressed as decimal degrees.  Negative values are used for longitudes west of 
+Greenwich so the allowable range for this argument is from -180 to 180.  
+Defaults to 75.76, the longitude of avantika.
+
+=back
 
 =cut
 
@@ -94,7 +181,7 @@ sub new {
             },
             tithi => {
                 type      => SCALAR,
-                default   => 0,
+                default   => 1,
                 callbacks => {
                     'between 1 and 14, or 15 or 30' => sub {
                         ( $_[0] > 0 && $_[0] < 15 )
@@ -286,9 +373,9 @@ sub _lunar_from_fixed {
     return $result;
 }
 
-=head2 clone
+=head3 clone
 
-TODO
+Returns a copy of the object.
 
 =cut
 
@@ -297,9 +384,12 @@ sub clone {
     return bless { %{$self} }, ref $self;
 }
 
-=head2 from_object
+=head3 from_object
 
-TODO
+Builds a C<DateTime::Calendar::Chandramana> object from another I<DateTime> 
+object.  This function takes an C<object> parameter and optionally C<locale>. 
+For example:
+
 
 =cut
 
@@ -356,17 +446,108 @@ sub from_object {
     return $newobj;
 }
 
-my %formats = (
-    'x' =>
-      sub { join( q{ }, $_[0]->varsha, $_[0]->masa_name, $_[0]->udita_tithi, ) }
-    ,
-);
+=head3 strftime(@formats)
 
-=head2 strftime(@formats)
+This function takes one or more parameters consisting of strings
+containing special specifiers.  For each such string it will return a
+string formatted according to the specifiers, er, specified.  The following
+specifiers are allowed in the format string:
 
-TODO
+=over 4
+
+=item * %a Equivalent to L<adhikamasa_abbrev>.
+
+=item * %A Equivalent to L<adhikamasa_name>.
+
+=item * %l Equivalent to L<adhikatithi_abbrev>.
+
+=item * %L Equivalent to L<adhikatithi_name>.
+
+=item * %m Equivalent to L<masa_abbrev>.
+
+=item * %M Equivalent to L<masa_name>.
+
+=item * %p Equivalent to L<paksha_abbrev>.
+
+=item * %P Equivalent to L<paksha_name>.
+
+=item * %t Equivalent to L<tithi_abbrev>.
+
+=item * %T Equivalent to L<tithi_name>.
 
 =cut
+
+#=item * %v Equivalent to L<vara_abbrev>.
+#
+#=item * %V Equivalent to L<vara_name>.
+#
+
+=item * %w Equivalent to L<tithi>.
+
+=item * %x Equivalent to '%y %A %M %P %L %w'
+
+=item * %y Equivalent to L<varsha>.
+
+=cut
+
+#=item * %Y Equivalent to L<varsha_name>.
+#
+
+=item * %% A literal `%' character.
+
+=back 
+
+Any method name may be specified using the format C<%{method}> name
+where "method" is a valid C<DateTime::Calendar::Chandramana> object method.
+
+=cut
+
+my %formats = (
+
+    'a' => sub { $_[0]->adhikamasa_abbrev },
+
+    'A' => sub { $_[0]->adhikamasa_name },
+
+    'l' => sub { $_[0]->adhikatithi_abbrev },
+
+    'L' => sub { $_[0]->adhikatithi_name },
+
+    'm' => sub { $_[0]->masa_abbrev },
+
+    'M' => sub { $_[0]->masa_name },
+
+    'p' => sub { $_[0]->paksha_abbrev },
+
+    'P' => sub { $_[0]->paksha_name },
+
+    't' => sub { $_[0]->tithi_abbrev },
+
+    'T' => sub { $_[0]->tithi_name },
+
+    #    'v' => sub { $_[0]->vara_abbrev },
+    #
+    #    'V' => sub { $_[0]->vara_name },
+    #
+    'w' => sub { $_[0]->tithi },
+
+    'x' => sub {
+        join q{},
+          (
+            $_[0]->varsha . q{ },
+            $_[0]->adhikamasa ? ( $_[0]->adhikamasa_name . q{ } ) : q{},
+            $_[0]->masa_name . q{ },
+            $_[0]->paksha_name . q{ },
+            $_[0]->adhikatithi ? ( $_[0]->adhikatithi_name . q{ } ) : q{},
+            $_[0]->tithi,
+          );
+    },
+
+    'y' => sub { $_[0]->varsha },
+
+    #    'Y' => sub { $_[0]->varsha_name },
+
+    '%' => sub { q{%} },    ## no critic 'ProhibitNoisyQuotes'
+);
 
 sub strftime {
     my $self = shift;
@@ -403,9 +584,10 @@ sub strftime {
     return @r;
 }
 
-=head2 utc_rd_values
+=head3 utc_rd_values
 
-TODO
+Returns a three-element array containing the current UTC RD days, seconds, and 
+nanoseconds.  See L<DateTime> for more details.
 
 =cut
 
@@ -421,23 +603,11 @@ sub utc_rd_values {
     return ( $self->{rd_days}, $self->{rd_secs}, $self->{rd_nanosecs} || 0 );
 }
 
-=head2 _era
-
-TODO
-
-=cut
-
 sub _era {
     my ($self) = @_;
 
     return 0;
 }
-
-=head2 _masa_offset
-
-TODO
-
-=cut
 
 sub _masa_offset {
     my ($self) = @_;
@@ -445,21 +615,25 @@ sub _masa_offset {
     return 1;
 }
 
-=head2 _purnimanta
-
-TODO
-
-=cut
-
 sub _purnimanta {
     my ($self) = @_;
 
     return 0;
 }
 
-=head2 varsha
+#sub _varsha_name_style {
+#    my($self) = @_;
+#
+#    return 0;
+#}
 
-TODO
+=head2 UNITS OF TIME
+
+These methods return various parts of a chandramAna date.
+
+=head3 varsha
+
+Returns the varSha.
 
 =cut
 
@@ -469,33 +643,258 @@ sub varsha {
     return $self->{varsha};
 }
 
-=head2 masa_name
+#=head3 varsha_name
+#
+#TODO
+#
+#=cut
+#
+#sub varsha_name {
+#    my ($self) = @_;
+#
+#    my $ky = $self->{varsha} + $self->era;
+#    if ($self->{masa} >= $self->masa_offset) {
+#        $ky++;
+#    }
+#    my $barhaspatya_varsha = ($self->_varsha_name_style
+#        ? ($ky + ($ky / 86) + 25) # northern
+#        : ($ky + 12)              # southern
+#    ) % 60;
+#    return $varsha_nama[$barhaspatya_varsha];
+#}
 
-TODO
+=head3 adhikamasa
+
+Returns 1 if this is an adhikamAsa or 0 if it is not.
+
+=cut
+
+sub adhikamasa {
+    my ($self) = @_;
+
+    return $self->{adhikamasa};
+}
+
+=head3 adhikamasa_abbrev
+
+Returns the abbreviated adhikamAsa name.  (By default 'a '.)
+
+=cut
+
+sub adhikamasa_abbrev {
+    my ($self) = @_;
+
+    return ( $self->{adhikamasa} ? $adhika_abbrev : undef );
+}
+
+=head3 adhikamasa_name
+
+Returns the full adhikamAsa name. (By default 'adhika '.)
+
+=cut
+
+sub adhikamasa_name {
+    my ($self) = @_;
+
+    return ( $self->{adhikamasa} ? $adhika_nama : undef );
+}
+
+=head3 masa
+
+Returns the mAsa as a number from 1 to 12
+
+=cut
+
+sub masa {
+    my ($self) = @_;
+
+    return $self->{masa};
+}
+
+=head3 masa_abbrev
+
+Returns the abbreviated mAsa name.
+
+=cut
+
+sub masa_abbrev {
+    my ($self) = @_;
+
+    return $masa_abbrev[ $self->{masa} - 1 ];
+}
+
+=head3 masa_name
+
+Returns the full mAsa name.
 
 =cut
 
 sub masa_name {
     my ($self) = @_;
 
-    return ( $self->{adhikamasa} ? "$adhika_nama " : q{} )
-      . $masa_nama[ $self->{masa} - 1 ];
+    return $masa_nama[ $self->{masa} - 1 ];
 }
 
-=head2 udita_tithi
+=head3 paksha
 
-TODO
+Returns 1 if this is the kR^iShNapakSha or 0 if it is the shuklapakSha.
 
 =cut
 
-sub udita_tithi {
+sub paksha {
     my ($self) = @_;
 
-    return
-        $paksha_nama[ $self->{paksha} ] . q{ }
-      . ( $self->{adhikatithi} ? "$adhika_nama " : q{} )
-      . $self->{tithi};
+    return $self->{paksha};
 }
+
+=head3 paksha_abbrev
+
+Returns the abbreviated pakSha name.  By default either 'shu' for shukla or 
+'kR^i' for kR^iShNa.
+
+=cut
+
+sub paksha_abbrev {
+    my ($self) = @_;
+
+    return $paksha_abbrev[ $self->{paksha} ];
+}
+
+=head3 paksha_name
+
+Returns the full paksha name. By default either shukla or kR^iShNa.
+
+=cut
+
+sub paksha_name {
+    my ($self) = @_;
+
+    return $paksha_nama[ $self->{paksha} ];
+}
+
+=head3 adhikatithi
+
+Returns 1 if this is an adhikatithi or 0 if it is not.
+
+=cut
+
+sub adhikatithi {
+    my ($self) = @_;
+
+    return $self->{adhikatithi};
+}
+
+=head3 adhikatithi_abbrev
+
+Returns the abbreviated adhikatithi name.  (By default 'a '.)
+
+=cut
+
+sub adhikatithi_abbrev {
+    my ($self) = @_;
+
+    return ( $self->{adhikatithi} ? $adhika_abbrev : undef );
+}
+
+=head3 adhikatithi_name
+
+Returns the full adhikatithi name. (By default 'adhika '.)
+
+=cut
+
+sub adhikatithi_name {
+    my ($self) = @_;
+
+    return ( $self->{adhikatithi} ? $adhika_nama : undef );
+}
+
+=head3 tithi
+
+Returns the tithi as a decimal number.
+
+=cut
+
+sub tithi {
+    my ($self) = @_;
+
+    return $self->{tithi};
+}
+
+=head3 tithi_abbrev
+
+Returns the abbreviated tithi name.
+
+=cut
+
+sub tithi_abbrev {
+    my ($self) = @_;
+
+    if ( $self->{tithi} == 15 ) {
+        return $purnima_abbrev;
+    }
+
+    if ( $self->{tithi} == 30 ) {
+        return $amavasya_abbrev;
+    }
+
+    return $tithi_abbrev[ $self->{tithi} - 1 ];
+}
+
+=head3 tithi_name
+
+Returns the full tithi name.
+
+=cut
+
+sub tithi_name {
+    my ($self) = @_;
+
+    if ( $self->{tithi} == 15 ) {
+        return $purnima_nama;
+    }
+
+    if ( $self->{tithi} == 30 ) {
+        return $amavasya_nama;
+    }
+
+    return $tithi_nama[ $self->{tithi} - 1 ];
+}
+
+#=head3 vara
+#
+#returns the day of the week as a decimal number.
+#
+#=cut
+#
+#sub vara {
+#    my ($self) = @_;
+#
+#    return $self->{vara};
+#}
+#
+#=head3 vara_abbrev
+#
+#returns the abbreviated name of the current day of the week.
+#
+#=cut
+#
+#sub vara_abbrev {
+#    my ($self) = @_;
+#
+#    return $vara_abbrev[ $self->{vara} ];
+#}
+#
+#=head3 vara_name
+#
+#returns the name of the current day of the week.
+#
+#=cut
+#
+#sub vara_name {
+#    my ($self) = @_;
+#
+#    return $vara_nama[ $self->{vara} ];
+#}
 
 =head1 AUTHOR
 
@@ -551,3 +950,4 @@ under the same terms as Perl itself.
 =cut
 
 1;    # End of DateTime::Indic::Chandramana
+
